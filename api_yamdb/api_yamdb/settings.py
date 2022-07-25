46e291ae0fe9558aa88455006760ff9d063a1efb
egorcoders <egorcoders@gmail.com>
2022-07-25 19:41:40 +0500

    modified:   api_yamdb/api_yamdb/settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -100,7 +100,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 START_YEAR = 1600
 
